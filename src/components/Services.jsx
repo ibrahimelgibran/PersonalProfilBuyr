a384b2429d6b3ed6e3af63e18ff39a5c434690c6
ElGibran17 <ibrahimelgibran17@gmail.com>
2023-11-10 21:00:41 +0700

    update

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -9,14 +9,14 @@ const Services = () => {
         Our features:
       </h1>
       <div className="grid md:grid-cols-3 my-8 gap-5">
-        {services.map((service) => (
+        {services.map((services) => (
           <div
-            key={service.id}
+            key={services.id}
             className="service-card flex flex-col justify-center items-center p-6 text-center text-white"
           >
-            <img src={service.image} alt="" className="mb-3" />
-            <h6 className="text-2xl mb-4">{service.name}</h6>
-            <p className="text-base">{service.description}</p>
+            <img src={services.image} alt="" className="mb-3" />
+            <h6 className="text-2xl mb-4">{services.name}</h6>
+            <p className="text-base">{services.description}</p>
           </div>
         ))}
       </div>
